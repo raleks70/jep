573aced9e5d9e9b0b13c69e612ced8fe2c8d794e
unknown <rpiirto@FISK-RPIIRTO1L1.tellabs-east.tellabsinc.net>
2012-11-08 15:33:37 +0200

    Checkkaa eka file

---
+++ AccessAse.pl
@@ -0,0 +1,19 @@
+#!/usr/bin/perl
+
+use DBI;
+use DBD::Sybase;
+
+$dbh = DBI->connect('DBI:Sybase:server=FISK0810;database=pslab', 'sa', 'ngpadmin',
+            { RaiseError => 1, AutoCommit => 1});
+#$dbh->do($SQL);
+$sth = $dbh->prepare("select nid, nnm, nhwtype from node");
+$sth->execute();
+while (@row = $sth->fetchrow_array) {
+    foreach $_ (@row) {
+		print $_, "\n";
+    }
+}
+$sth->finish();
+$dbh->disconnect();
+
+
